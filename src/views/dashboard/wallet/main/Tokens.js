ee6218924e6ee1a518735555097dcdd694b9ef1d
J-Gambler <anotovserg943@gmail.com>
2022-01-17 19:35:27 +0300

    Tokens list page beta version

--- src/views/dashboard/wallet/main/Tokens.js
+++ src/views/dashboard/wallet/main/Tokens.js
@@ -4,19 +4,23 @@ import { useSelector } from 'react-redux';
 import { useTheme } from '@emotion/react';
 
 import CloseIcon from '@mui/icons-material/Close';
-import BinanceIcon from 'assets/images/icons/binance-smart-chain.png';
-import EtherIcon from 'assets/images/icons/eth.png';
+import EtherIcon from 'assets/images/icons/Eth.svg';
+import Bitcoin from 'assets/images/icons/Bitcoin.svg';
+import XPRcoin from 'assets/images/icons/XPR.svg';
+
+import UpArrow from 'assets/images/icons/UpArrow.svg';
+import DownArrow from 'assets/images/icons/DownArrow.svg';
 
 const Tokens = () => {
 
     const tokens = [
-        {coin: 'Bitcoin',img: BinanceIcon,unit: 'BTS'},
-        {coin: 'Ethereum',img: EtherIcon,unit: 'ETH'},
-        {coin: 'XPR',img: EtherIcon,unit: 'BNB'},
-        {coin: 'Bitcoin',img: EtherIcon,unit: 'BTS'},
-        {coin: 'Ethereum',img: EtherIcon,unit: 'ETH'},
-        {coin: 'XPR',img: EtherIcon,unit: 'BNB'},
-        {coin: 'Bitcoin',img: EtherIcon,unit: 'BTS'}
+        {coin: 'Bitcoin',img: Bitcoin,unit: 'BTS', bg: 'rgba(255, 210, 51, 0.15)', p: '13px', arrow: UpArrow, arrowColor: '#60BA62' },
+        {coin: 'Ethereum',img: EtherIcon,unit: 'ETH', bg: '#7F6EE9', p: '13px', arrow: DownArrow, arrowColor: '#ED2F00'},
+        {coin: 'XPR',img: XPRcoin,unit: 'BNB', bg: '#25292C', p: '13px', arrow: UpArrow, arrowColor: '#60BA62'},
+        {coin: 'Bitcoin',img: Bitcoin ,unit: 'BTS', bg: 'rgba(255, 210, 51, 0.15)', p: '13px', arrow: UpArrow, arrowColor: '#60BA62'},
+        {coin: 'Ethereum',img: EtherIcon,unit: 'ETH', bg: '#7F6EE9', p: '13px', arrow: DownArrow, arrowColor: '#ED2F00'},
+        {coin: 'XPR',img: XPRcoin,unit: 'BNB', bg: '#25292C', p: '13px', arrow: UpArrow, arrowColor: '#60BA62'},
+        {coin: 'Bitcoin',img: Bitcoin ,unit: 'BTS', bg: 'rgba(255, 210, 51, 0.15)', p: '13px', arrow: UpArrow, arrowColor: '#60BA62'}
     ]
     const customization = useSelector((state) => state.customization);
     const theme = useTheme();
@@ -43,13 +47,20 @@ const Tokens = () => {
                                     <CardHeader 
                                         sx={{ p: 0, '& .css-sgoict-MuiCardHeader-action': {m: 'auto'} }}
                                         avatar={
-                                            <Avatar aria-label="recipe" src={e.img} />
+                                            <Avatar aria-label="recipe" src={e.img} sx={{ p: '8px', px: e.p, bgcolor: e.bg }} />
                                         }
-                                        title={<Typography variant="h5">{e.coin}</Typography>}
-                                        subheader={<Typography sx={{ fontSize: '.75rem' }}>$43,678.54</Typography>}
+                                        title={<Typography sx={{ fontSize: '14px', fontWeight: 500, color: 'black' }}>{e.coin}</Typography>}
+                                        subheader={
+                                            <Typography component="div">
+                                                <Typography sx={{ display: 'inline-block', fontSize: '11px', fontWeight: 500, color: 'rgba(22, 37, 52, 0.5)' }}>$43,678.54</Typography>
+                                                <Typography component="div" sx={{ verticalAlign: 'middle', display: 'inline-block', pl: '7px' }}>
+                                                    <img src={e.arrow} style={{ width: '11px' }} />
+                                                </Typography>
+                                                <Typography sx={{ display: 'inline-block', color: e.arrowColor, fontSize: '11px' }}>$12.00</Typography>
+                                            </Typography>}
                                         action={
                                             <Typography component="div" sx={{ display: 'flex' }}>
-                                                <Typography sx={{ display: 'inline-block', m: 'auto', fontSize: '1rem', color: '#162534'}} variant="h4">{e.unit}</Typography>
+                                                <Typography sx={{ display: 'inline-block', m: 'auto', fontSize: '1rem', color: '#162534'}} variant="h4">0 {e.unit}</Typography>
                                                 <Divider orientation="vertical" flexItem sx={{ borderColor: '#cbd0d4', mx: '.5rem' }} />
                                                 <CloseIcon sx={{ m: 'auto', color: '#cbd0d4' }}/>
                                             </Typography>
