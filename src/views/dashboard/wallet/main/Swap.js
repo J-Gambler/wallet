99147276eadcb2ad1af2767ef31ba1944c2f5a7c
J-Gambler <anotovserg943@gmail.com>
2022-01-17 21:34:49 +0300

    swap modal beta version

--- src/views/dashboard/wallet/main/Swap.js
+++ src/views/dashboard/wallet/main/Swap.js
@@ -12,7 +12,8 @@ import {
     CardHeader
 } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import User1 from 'assets/images/users/user-round.png';
+import EtherIcon from 'assets/images/icons/Eth.svg';
+import BitcoinTemp  from 'assets/images/icons/BitcoinTemp.svg';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
     
 const Swap = ({ onClose, open }) => {
@@ -28,111 +29,131 @@ const Swap = ({ onClose, open }) => {
             onClose={handleClose} 
             open={open}
         >
-            <Box sx={{ p: '2rem', width: '500px' }}>
-                <Typography sx={{ textAlign: 'center' }} variant="h2">Swap</Typography>
-            </Box>
-            <Box sx={{ px: '1rem', pb: '3rem', flexDirection: 'column', gap: '.5rem', display: 'flex' }}>
-                <Box 
-                    sx={{ 
-                        display: 'flex', 
-                        flexDirection: 'row', 
-                        justifyContent: 'space-between', 
-                        bgcolor: 'white',
-                        borderRadius: `${customization.borderRadius}px`,
-                        border: 2,
-                        borderColor: 'white',
-                        '&:hover': {
-                            cursor: 'pointer',
-                            borderColor: '#7c66eb'
-                        }
-                    }}
-                >
-                    <Typography component="div" 
+            <Typography component="div" sx={{ bgcolor: '#F8FAFC'}}>
+                <Box sx={{ p: '2rem', width: '500px' }}>
+                    <Typography sx={{ textAlign: 'center' }} variant="h2">Swap</Typography>
+                </Box>
+                <Box sx={{ px: '1rem', pb: '3rem', flexDirection: 'column', gap: '.5rem', display: 'flex' }}>
+                    <Box 
                         sx={{ 
-                            p: '1.25rem', 
-                            display: 'inline-block' 
+                            display: 'flex', 
+                            flexDirection: 'row', 
+                            justifyContent: 'space-between', 
+                            bgcolor: 'white',
+                            borderRadius: `${customization.borderRadius}px`,
+                            borderColor: 'white',
+                            '&:hover': {
+                                cursor: 'pointer',
+                                border: 2,
+                                borderColor: '#7c66eb'
+                            }
                         }}
                     >
-                        <Typography>EXCHANGE</Typography>
-                        <Typography variant="h4">400</Typography>
+                        <Typography component="div" 
+                            sx={{ 
+                                p: '1.25rem', 
+                                display: 'inline-block' 
+                            }}
+                        >
+                            <Typography sx={{ fontSize: '12px'}}>EXCHANGE</Typography>
+                            <Typography variant="h4" sx={{ fontSize: '18px', pt: '.25rem' }}>400</Typography>
+                        </Typography>
+                        <Typography 
+                            component="div" 
+                            sx={{ 
+                                my: 'auto',
+                                width: '200px',
+                                bgcolor: '#ecf8ff',
+                                p: '27px',
+                                borderRadius: `0px ${customization.borderRadius}px ${customization.borderRadius}px 0px`,
+                            }}
+                        >
+                            
+                            <CardHeader 
+                                sx={{ p: 0, 
+                                    '& .MuiCardHeader-action' : {
+                                        m: '0px', ml: 'auto', mt: 'auto'
+                                } }}
+                                avatar={
+                                    <Avatar aria-label="recipe" src={EtherIcon} sx={{ width: '32px', height: '32px', bgcolor: '#7F6EE9', p: '8px', py: '12px' }} />
+                                }
+                                title={<Typography variant="h3" sx={{ bgcolor: 'transparent', fontSize: '16px' }}>Alex S.</Typography>}
+                                action={
+                                    <KeyboardArrowDownIcon sx={{ fontSize: '1.5rem' }} />
+                                }
+                            />
+                        </Typography>
+                    </Box>
+                    <Box 
+                        sx={{ 
+                            display: 'flex', 
+                            flexDirection: 'row', 
+                            justifyContent: 'space-between', 
+                            bgcolor: 'white',
+                            borderRadius: `${customization.borderRadius}px`,
+                            borderColor: 'white',
+                            '&:hover': {
+                                cursor: 'pointer',
+                                border: 2,
+                                borderColor: '#7c66eb'
+                            }
+                        }}
+                    >
+                        <Typography 
+                            component="div" 
+                            sx={{ 
+                                display: 'inline-block',
+                                p: '1.25rem'
+                            }}
+                        >
+                            <Typography sx={{ fontSize: '12px'}}>RECEIVE</Typography>
+                            <Typography variant="h4" sx={{ fontSize: '18px', pt: '.25rem' }}>~0.3468</Typography>
+                        </Typography>
+                        <Typography 
+                            component="div"
+                            sx={{ 
+                                my: 'auto',
+                                width: '200px',
+                                bgcolor: '#ecf8ff',
+                                p: '27px',
+                                borderRadius: `0px ${customization.borderRadius}px ${customization.borderRadius}px 0px`,
+                            }}
+                        >
+                            <CardHeader 
+                                sx={{ p: 0, 
+                                    '& .MuiCardHeader-action' : {
+                                        m: '0px', ml: 'auto', mt: 'auto'
+                                } }}
+                                avatar={
+                                    <Avatar aria-label="recipe" src={BitcoinTemp} sx={{ width: '32px', height: '32px', p: '8px', bgcolor: '#FFAF50' }} />
+                                }
+                                title={<Typography variant="h3" sx={{ bgcolor: 'transparent', fontSize: '16px' }}>BTC</Typography>}
+                                action={
+                                    <KeyboardArrowDownIcon sx={{ fontSize: '1.5rem' }} />
+                                }
+                            />
+                        </Typography>
+                    </Box>
+                    <Typography sx={{ textAlign: 'center', p: 1, color: 'black' }}>
+                        1 BTC = ~143.23 ETH
                     </Typography>
-                    <Typography 
-                        component="div" 
-                        sx={{ 
-                            width: '200px',
-                            bgcolor: '#ecf8ff',
-                            p: '1.25rem',
-                            borderRadius: `0px ${customization.borderRadius}px ${customization.borderRadius}px 0px`,
-                        }}
-                    >
-                        
-                        <CardHeader 
-                            sx={{ p: 0, '& .css-sgoict-MuiCardHeader-action': {m: 'auto'} }}
-                            avatar={
-                                <Avatar aria-label="recipe" src={User1} />
-                            }
-                            title={<Typography variant="h3" sx={{ bgcolor: 'transparent' }}>Alex S.</Typography>}
-                            action={
-                                <KeyboardArrowDownIcon sx={{ fontSize: '1.5rem', ml: 1 }} />
-                            }
-                        />
+                    <Typography component="div">
+                        <Button 
+                            variant="contained" 
+                            sx={{ width: '100%', 
+                                bgcolor: '#7C66EB',
+                                borderRadius: '4px',
+                                paddingTop: '12.5px', 
+                                paddingBottom: '12.5px', 
+                                color: '#FFFFFF', 
+                                fontSize: '16px',  
+                            }}
+                        >
+                            Swap
+                        </Button>
                     </Typography>
                 </Box>
-                <Box 
-                    sx={{ 
-                        display: 'flex', 
-                        flexDirection: 'row', 
-                        justifyContent: 'space-between', 
-                        bgcolor: 'white',
-                        borderRadius: `${customization.borderRadius}px`,
-                        border: 2,
-                        borderColor: 'white',
-                        '&:hover': {
-                            cursor: 'pointer',
-                            borderColor: '#7c66eb'
-                        }
-                    }}
-                >
-                    <Typography 
-                        component="div" 
-                        sx={{ 
-                            display: 'inline-block',
-                            p: '1.25rem'
-                        }}
-                    >
-                        <Typography>YOU RECEIVE</Typography>
-                        <Typography variant="h4">~0.346838568434</Typography>
-                    </Typography>
-                    <Typography 
-                        component="div"
-                        sx={{ 
-                            width: '200px',
-                            bgcolor: '#ecf8ff',
-                            p: '1.25rem',
-                            borderRadius: `0px ${customization.borderRadius}px ${customization.borderRadius}px 0px`,
-                        }}
-                    >
-                        <CardHeader 
-                            sx={{ p: 0, '& .css-sgoict-MuiCardHeader-action': {m: 'auto'} }}
-                            avatar={
-                                <Avatar aria-label="recipe" src={User1} />
-                            }
-                            title={<Typography variant="h3" sx={{ bgcolor: 'transparent' }}>BTC</Typography>}
-                            action={
-                                <KeyboardArrowDownIcon sx={{ fontSize: '1.5rem', ml: 1 }} />
-                            }
-                        />
-                    </Typography>
-                </Box>
-                <Typography sx={{ textAlign: 'center', p: 1 }}>
-                    1 BTC = ~143.23 ETH
-                </Typography>
-                <Typography component="div">
-                    <Button variant="contained" sx={{ width: '100%', bgcolor: '#7c66eb', py: 1 }}>
-                        Swap
-                    </Button>
-                </Typography>
-            </Box>
+            </Typography>
         </Dialog>
     );
 }
